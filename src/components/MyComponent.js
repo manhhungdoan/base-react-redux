373f5cbdf3f43784f2b90e792ee1023f0c6bb598
mhung <hungthiendoan@gmail.com>
2022-08-14 13:29:46 +0700

    first commit

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    //JSX
+    render() {
+        return (
+            <div>
+                My first Component
+            </div> 
+        );
+    }
+}
+
+export default MyComponent;
