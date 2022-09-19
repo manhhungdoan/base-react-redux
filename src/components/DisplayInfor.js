2e007047c36892420c6b23b5f7f6a6787ab4fa1a
mhung <hungthiendoan@gmail.com>
2022-09-19 22:18:45 +0700

    stateLess/stateFul(class => funtion

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,78 +1,102 @@
 import React from "react";
 import './DisplayInfor.scss';
 // import logo from './../logo.svg';
-class DisplayInfor extends React.Component {
+// class DisplayInfor extends React.Component {
 
-    //oop style
-    constructor(props) {
-        console.log('>> call constructor 1');
-        super(props);
-        this.state = {
-            isShowList: true
-        }
-    }
-    //babel compiler
-    // state = {
-    //     isShowList: true
-    // }
+//     //oop style
+//     // constructor(props) {
+//     //     console.log('>> call constructor 1');
+//     //     super(props);
+//     //     this.state = {
+//     //         isShowList: true
+//     //     }
+//     // }
+//     //babel compiler
+//     // state = {
+//     //     isShowList: true
+//     // }
 
-    componentDidMount() {
-        console.log('>>Call me component did mount');
-        setTimeout(() => {
-            document.title('mhung did mount');
-        }, 3000);
-    }
+//     // handleShowHide = () => {
+//     //     this.setState({
+//     //         isShowList: !this.state.isShowList
+//     //     })
+//     // }
+//     // deleteUser = (id) => {
+//     //     this.props.handleDeleteUser(id);
+//     // }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log('>>call me comp did update', this.props, prevProps)
-        if (this.props.listUsers !== prevProps.listUsers) {
-            alert('you are 4')
-        }
-    }
-    handleShowHide = () => {
-        this.setState({
-            isShowList: !this.state.isShowList
-        })
-    }
-    deleteUser = (id) => {
-        this.props.handleDeleteUser(id);
-    }
-    render() {
-        console.log("call me render")
-        const { listUsers } = this.props;
-        const test_variables = { name: 'mhung', age: 18 }
-        return (
-            <>
-                {JSON.stringify(test_variables)}
-                <br></br>
-                <div className="display-infor-container">
-                    {/* <img src={logo} /> */}
-                    <div>
+//     //statekess(Khong co state) vs stateful(Co state)
+//     render() {
+//         console.log("call me render")
+//         const { listUsers } = this.props; //Nhan du lieu tu thang cha
+//         const test_variables = { name: 'mhung', age: 18 }
+//         return (
+//             <>
+//                 {JSON.stringify(test_variables)}
+//                 <br></br>
+//                 <div className="display-infor-container">
+//                     {/* <img src={logo} /> */}
+//                     {/* <div>
+//                         <button onClick={() => this.handleShowHide()}>{this.state.isShowList === true ? "Hide List Users" : "Show List Users"}</button>
+//                     </div> */}
+//                     {true &&
+//                         <div>
+//                             {
+//                                 listUsers.map((user) => {
+//                                     return (
+//                                         <div className={+user.age > 18 ? "green" : "red"}>
+//                                             My name: {user.name}
+//                                             <br></br>
+//                                             Age: {user.age}
+//                                             <div>
+//                                                 <button onClick={() => this.deleteUser(user.id)}>Delete</button>
+//                                             </div>
+//                                             <hr></hr>
+//                                         </div>
+//                                     )
+//                                 }
+//                                 )
+//                             }
+//                         </div>
+//                     }
+//                 </div>
+//             </>
+//         )
+//     }
+// }
+
+const DisplayInfor = (props) => {
+    const { listUsers } = props;//object
+    return (
+        <>
+            <br></br>
+            <div className="display-infor-container">
+                {/* <img src={logo} /> */}
+                {/* <div>
                         <button onClick={() => this.handleShowHide()}>{this.state.isShowList === true ? "Hide List Users" : "Show List Users"}</button>
-                    </div>
-                    {this.state.isShowList &&
-                        <div>
-                            {
-                                listUsers.map((user) => {
-                                    return (
-                                        <div className={+user.age > 18 ? "green" : "red"}>
-                                            My name: {user.name}
-                                            <br></br>
-                                            Age: {user.age}
-                                            <div>
-                                                <button onClick={() => this.deleteUser(user.id)}>Delete</button>
-                                            </div>
-                                            <hr></hr>
+                    </div> */}
+                {true &&
+                    <div>
+                        {
+                            listUsers.map((user) => {
+                                return (
+                                    <div className={+user.age > 18 ? "green" : "red"}>
+                                        My name: {user.name}
+                                        <br></br>
+                                        Age: {user.age}
+                                        <div>
+                                            <button onClick={() => props.handleDeleteUser(user.id)}>Delete</button>
                                         </div>
-                                    )
-                                }
+                                        <hr></hr>
+                                    </div>
                                 )
                             }
-                        </div>
-                    }
-                </div>
-            </>
-        )
-    }
+                            )
+                        }
+                    </div>
+                }
+            </div>
+        </>
+    )
 }
 export default DisplayInfor;
