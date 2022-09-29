822bdab1a05ba528264699a25627d7bcf481ba20
mhung <hungthiendoan@gmail.com>
2022-09-29 22:08:07 +0700

    #51 index routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">MHUNG</Navbar.Brand>
+                <NavLink to='/' className='navbar-brand'>MHUNG</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -17,9 +17,9 @@ const Header = () => {
                     </Nav>
                     <nav>
                         <NavDropdown title="Settings" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Logout</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+                            <NavDropdown.Item >Login</NavDropdown.Item>
+                            <NavDropdown.Item >Logout</NavDropdown.Item>
+                            <NavDropdown.Item >Profile</NavDropdown.Item>
                         </NavDropdown>
                     </nav>
                 </Navbar.Collapse>
---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,9 @@
+const HomePage = (props) => {
+    return (
+        <div className="homepage-container">
+            Home page container
+        </div>
+    )
+}
+
+export default HomePage;
