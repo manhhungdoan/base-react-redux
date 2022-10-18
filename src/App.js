860bc64211986761b04df0304e95475cee7404af
mhung <hungthiendoan@gmail.com>
2022-10-18 16:31:29 +0700

    about redux

--- src/App.js
+++ src/App.js
@@ -4,35 +4,37 @@ import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/MyComponent';
 import React from 'react';
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <MyComponent />
-      </div>
-    )
-  }
-}
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div>
-//       Hello World;
-//       <MyComponent />
-//     </div>
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <p>
-//     //       Edit <code>src/App.js</code> and save to reload.
-//     //     </p>
-//     //     <div>Count = {count}</div>
-//     //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//     //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//     //   </header>
-//     // </div>
-//   );
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <MyComponent />
+//       </div>
+//     )
+//   }
 // }
+const App = () => {
+  const count = useSelector(state => state.counter.count);
+  //stateRedux => call Reducer => state's reducer
+  //state redux changes => react changes
+  const dispatch = useDispatch();
+
+  return (
+    // <div>
+    //   Hello World;
+    //   <MyComponent />
+    // </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>Count = {count}</div>
+        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+      </header>
+    </div>
+  );
+}
 export default App;
