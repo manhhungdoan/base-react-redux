026ee6a2161f0817cdc15a476b50872ea2756080
mhung <hungthiendoan@gmail.com>
2022-11-29 00:54:46 +0700

    #83 Process Data in FE

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from '../../services/apiServices';
+import _ from 'lodash';
+
 const DetailQuiz = (props) => {
     const params = useParams();
 
@@ -12,7 +14,30 @@ const DetailQuiz = (props) => {
 
     const fetchQuestion = async () => {
         let res = await getDataQuiz(quizId);
-        console.log("check question: ", res);
+        if (res && res.EC === 0) {
+            let data = res.DT;
+            let raw =
+                _.chain(data)
+                    // Group the elements of Array based on `color` property
+                    .groupBy("id")
+                    // `key` is group's name (color), `value` is the array of objects
+                    .map((value, key) => {
+                        let answers = [];
+                        let questionDescription, image = null;
+                        value.forEach((item, index) => {
+                            if (index === 0) {
+                                questionDescription = item.description;
+                                image = item.image;
+                            }
+                            answers.push(item.answers);
+                        })
+                        return { questionId: key, answers, questionDescription, image }
+                    })
+                    .value()
+            console.log("data", data)
+            console.log("group data: ", raw)
+        }
+
     }
     return (
         <div className="detail-quiz-container">
